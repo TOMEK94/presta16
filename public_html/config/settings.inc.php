6993a9dabd234a0604eccc9d0f8890cfb210ebcd
TOMEK94 <tomaszpazik@gmail.com>
2020-01-16 14:28:35 +0100

    Prestashop 1.6

---
+++ public_html/config/settings.inc.php
@@ -0,0 +1,16 @@
+<?php
+define('_DB_SERVER_', 'mysql');
+define('_DB_NAME_', 'krulove24_admin');
+define('_DB_USER_', 'krulove24_admin');
+define('_DB_PASSWD_', 'admin');
+define('_DB_PREFIX_', 'ps_');
+define('_MYSQL_ENGINE_', 'InnoDB');
+define('_PS_CACHING_SYSTEM_', 'CacheMemcache');
+define('_PS_CACHE_ENABLED_', '0');
+define('_COOKIE_KEY_', 'eISdIDDYqVfBV7D6ogxeUD3T7uISZis918MYee2rKo4WzAfiMC2W7GBj');
+define('_COOKIE_IV_', 'XEL1LHTA');
+define('_PS_CREATION_DATE_', '2020-01-16');
+if (!defined('_PS_VERSION_'))
+	define('_PS_VERSION_', '1.6.1.15');
+define('_RIJNDAEL_KEY_', 'uRzQEMyAbgjkM8AxyZdfWzTCZaZS9xx6');
+define('_RIJNDAEL_IV_', 'JGF0xAmO4NAD+MkfjwKYRA==');
